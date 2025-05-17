e32c25bcb188e45b8b04e697855928f173c32e7c
Yin <sky921418@gmail.com>
2025-05-17 23:27:01 +0800

    update

--- YN/project/project/obj/Debug/net8.0/project.AssemblyInfo.cs
+++ YN/project/project/obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70dd98b506639ce28121a338f7451f9602916f6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c79919a8f69f6b090c94e3655cffca7ec5a5e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
